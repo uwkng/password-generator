275c253aae21c6819b1425e5480f329bd259b348
uwkng <koenig_u@gmx.net>
2025-07-30 19:48:00 +0200

    fix: fixed JS referencing problem

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-createPassword() {
+
+function createPassword() {
     let password = "";
     for (let i = 0; i < 12; i++) {
-        password += characters[Math.floor(Math.random() * characters.length)]
+        password += characters[Math.floor(Math.random() * characters.length)];
     }
-    document.getElementById("passwordOutput1").value = password
+    document.getElementById("passwordOutput1").textContent = password;
 }
+
